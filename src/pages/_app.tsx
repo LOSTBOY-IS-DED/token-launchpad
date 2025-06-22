8114295181371cfc4892fbe8b8066b7b2e7f5382
Subhajit Chaudhury <subhajitchaudhury05@gmail.com>
2025-06-22 22:58:22 +0530

    import few js lib to app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import { AppProps } from "next/app";
+import { Head } from "next/document";
+import { FC } from "react";
+import ContextProvider from "contexts/ContextProvider";
+import AppBar from "components/AppBar";
+import Footer from "components/Footer";
+import Notification from "components/Notification";
 
-const _app = () => {
-  return (
-    <div>_app</div>
+require("@solana/wallet-adapter-react-ui/styles.css");
+require("../styles/globals.css");
+
+const App :  FC<AppProps> =  ({Component , pageProps}) => {
+  return  (
+    <div className="bg-default-900">
+      <Head>
+        Solana Token Creator
+      </Head>
+      <ContextProvider>
+        <Notification/>
+        <Component {...pageProps} />
+        <Footer/>
+      </ContextProvider>
+
+
+      {/* SCRIPTS */}
+      <script src="assets/lib/preline/parline.js"></script>
+      <script src="assets/lib/swiper/swiper-bundle.js"></script>
+      <script src="assets/lib/gumshoe/gumshoe.polyfills.min.js"></script>
+      <script src="assets/lib/lucide/lucide.min.js"></script>
+      <script src="assets/lib/aos/aos.min.js"></script>
+      <script src="assets/js/swiper.js"></script>
+      <script src="assets/js/theme.js"></script>
+    </div>
   )
 }
 
-export default _app
+export default App;
